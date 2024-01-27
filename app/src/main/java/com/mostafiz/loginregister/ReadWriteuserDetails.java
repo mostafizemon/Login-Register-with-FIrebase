4388dc8d0795aa3ca48e0558c0ffc4027d14b8c9
mostafizemon <mostafizemon09@gmail.com>
2024-01-27 13:50:08 +0600

    login register process with firebase

--- app/src/main/java/com/mostafiz/loginregister/ReadWriteuserDetails.java
+++ app/src/main/java/com/mostafiz/loginregister/ReadWriteuserDetails.java
@@ -1,10 +1,11 @@
 package com.mostafiz.loginregister;
 
 public class ReadWriteuserDetails {
-    public String fullname,phone;
-    public ReadWriteuserDetails(String fullname,String phone){
+    public String fullname,phone,dob;
+    public ReadWriteuserDetails(String fullname,String phone,String dob){
         this.fullname=fullname;
         this.phone=phone;
+        this.dob=dob;
 
     }
 }
--- app/src/main/java/com/mostafiz/loginregister/RegisterActivity.java
+++ app/src/main/java/com/mostafiz/loginregister/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.mostafiz.loginregister;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.hardware.camera2.CameraExtensionSession;
 import android.os.Bundle;
@@ -10,6 +11,9 @@ import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +21,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
@@ -26,11 +31,15 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+
 public class RegisterActivity extends AppCompatActivity {
+    CheckBox registercheckbox;
     ProgressBar registerprogressbar;
-    TextInputEditText registerfullname, registeremail,registerphone,registerpassword;
+    TextInputEditText registerfullname, registeremail,registerphone,registerpassword,registerdob;
     TextView alreadyhaveanaccount;
     Button buttonregister;
+    DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +52,36 @@ public class RegisterActivity extends AppCompatActivity {
         registerphone=findViewById(R.id.registerphone);
         registerpassword=findViewById(R.id.registerpassword);
         registerprogressbar=findViewById(R.id.registerprogressbar);
+        registercheckbox=findViewById(R.id.registercheckbox);
+        registerdob=findViewById(R.id.registerdob);
+        registerdob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar=Calendar.getInstance();
+                int day=calendar.get(Calendar.DAY_OF_MONTH);
+                int month=calendar.get(Calendar.MONTH);
+                int year=calendar.get(Calendar.YEAR);
+                new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        registerdob.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                    }
+                },year,month,day).show();
+            }
+        });
 
         alreadyhaveanaccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
                 finish();
+            }
+        });
+        registercheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Enable registerButton if CheckBox is checked, disable it otherwise
+                buttonregister.setEnabled(isChecked);
             }
         });
 
@@ -59,6 +92,10 @@ public class RegisterActivity extends AppCompatActivity {
                 String email=registeremail.getText().toString();
                 String phone=registerphone.getText().toString();
                 String password=registerpassword.getText().toString();
+                String dob=registerdob.getText().toString();
+                if (dob.isEmpty()){
+                    registerdob.setError("Enter Your Date of Birth");
+                }
                 if (fullname.isEmpty()){
                     registerfullname.setError("Enter Your Full Name");
                     registerfullname.requestFocus();
@@ -75,10 +112,15 @@ public class RegisterActivity extends AppCompatActivity {
                     registerpassword.setError("Enter Your password");
                     registerpassword.requestFocus();
                 }
+                if (phone.length()!=11){
+                    registerphone.setError("Please Enter Your Valid Phone Number");
+                    registerphone.requestFocus();
+                }
+
                 else {
                     if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                         registerprogressbar.setVisibility(View.VISIBLE);
-                        registeruser(fullname, email, phone, password);
+                        registeruser(fullname, email, phone, password,dob);
                     }
                     else {
                         registeremail.setError("Please Enter Your Valid Email");
@@ -98,7 +140,7 @@ public class RegisterActivity extends AppCompatActivity {
 
 
 
-    private  void registeruser(String name,String email,String phone, String password){
+    private  void registeruser(String name,String email,String phone, String password,String dob){
         FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
         firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
@@ -107,7 +149,7 @@ public class RegisterActivity extends AppCompatActivity {
 
                     FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
 
-                    ReadWriteuserDetails readWriteuserDetails=new ReadWriteuserDetails(name,phone);
+                    ReadWriteuserDetails readWriteuserDetails=new ReadWriteuserDetails(name,phone,dob);
                     DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference("Users");
                     databaseReference.child(firebaseUser.getUid()).setValue(readWriteuserDetails).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
